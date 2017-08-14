d9b93ef194b7d4cfc4fadba7905149183314cb8a
root <root@server.artistry.host>
2017-08-14 15:29:28 -0400

    Latest

--- wp-content/plugins/segmentio/class.analytics.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! class_exists( 'Analytics' ) ) {
-	class Analytics extends Segment_IO_Analytics {
-		public function __construct() {
-			parent::__construct();
-		}
-	}	
-}
--- wp-content/plugins/segmentio/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit();
-}
-
-delete_option( 'analytics_wordpress_options' );
