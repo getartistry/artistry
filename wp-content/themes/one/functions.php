9186cd95c0658459b704e64d52cf20ab6937f93c
tonyoconnell <tony@one.ie>
2018-04-13 03:20:38 +0700

    proceed to checkout

--- wp-content/themes/one/functions.php
+++ wp-content/themes/one/functions.php
@@ -23,3 +23,31 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+/*- Translate -*/
+
+add_filter(  'gettext',  'wps_translate_words_array'  );
+add_filter(  'ngettext',  'wps_translate_words_array'  );
+function wps_translate_words_array( $translated ) {
+     $words = array(
+                        // 'word to translate' = > 'translation'
+                        'Permalink' => 'Link',
+                        'Related Products' => 'Related',
+                        'OUT OF STOCK' => 'Sold',
+                        'Howdy' => 'Hi',
+                        'Woocommerce Status' => 'Sales',
+                        'Woocommerce Recent Reviews' => 'Reviews',
+                        'Google Analytics Dashboard' => 'Analytics',
+                        'Add to cart' => 'Purchase',
+                    );
+     $translated = str_ireplace(  array_keys($words),  $words,  $translated );
+     return $translated;
+}
+
+/*- Redirect to checkout -*/
+
+add_filter ('woocommerce_add_to_cart_redirect', 'woo_redirect_to_checkout');
+function woo_redirect_to_checkout() {
+	$checkout_url = WC()->cart->get_checkout_url();
+	return $checkout_url;
+}
